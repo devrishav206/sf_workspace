270c7fd94153693f20e9e85358b8fa9a90a54589
devrishav206 <rishav.k.rathore@kipi.ai>
2025-11-11 16:38:13 +0530

    Update sf_branch.sql

--- sf_branch.sql
+++ sf_branch.sql
@@ -1,1 +1,4 @@
-This branch is created and pushed from snowflake workspace and 
+1. This branch is created and pushed from snowflake workspace
+
+2. this comment is added in github directly and will be pulled in sf workspace 
+
